c2b4994cb6cb8fdf829c321a8b9b6746a03953ec
Emre Çalışır <emrecalisir95@gmail.com>
2023-03-05 03:13:14 +0300

    Add admin page

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,46 @@
-from django.contrib import admin  # noqa = noqa yazınca flake8 bu hatayı geçer
+"""
+Django admin customization.
+"""
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _  # Gelecekte translate işlemlerini kolaylaştırmak için eklendi.
+
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ["id"]
+    list_display = ["email", "name"]
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),  # Burada None title koymadığımızı belirtiyor.
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                )
+            }
+        ),
+        (_("Important dates"), {"fields": ("last_login",)}),
+    )
+    readonly_fields = ["last_login"]
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide", ), # Custom CSS for the Django Admin Page
+            "fields": (
+                "email",
+                "password1",
+                "password2",
+                "name",
+                "is_active",
+                "is_staff",
+                "is_superuser",
+            )
+        }),  # tuple kullanmak gerekiyor virgülü unutma
+    )
+
+admin.site.register(models.User, UserAdmin)
