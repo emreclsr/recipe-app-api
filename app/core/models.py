e3c21b29d0ef3ccd4c74f0a2da1fe456be2dc1e5
Emre Çalışır <emrecalisir95@gmail.com>
2023-03-28 22:55:58 +0300

    Add ingredient model

--- app/core/models.py
+++ app/core/models.py
@@ -52,12 +52,25 @@ class Recipe(models.Model):  # models.Model -> Django base model
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField("Tag")  # many different recipes have many different tags
+    ingredients = models.ManyToManyField("Ingredient")
 
     def __str__(self):  # Özellikle django admin'de title şeklinde gösterilmesi için ayarlandı.
         return self.title
 
 class Tag(models.Model):
     """Tag for filtering recipes."""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    """Ingredient for recipes"""
     name = models.CharField(max_length=255)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
