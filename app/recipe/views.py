3957ff8471af7219dbd428da5aa2579d80c8452d
Emre Çalışır <emrecalisir95@gmail.com>
2023-03-30 01:59:22 +0300

    Update ingredient part

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -34,32 +34,28 @@ class RecipeViewSet(viewsets.ModelViewSet):  # ModelViewSet -> modelleri ile ça
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(mixins.DestroyModelMixin,  # delete
-                 mixins.UpdateModelMixin,  # update
-                 mixins.ListModelMixin,
-                 viewsets.GenericViewSet):  # mixins, generic viewset'ten önce tanımlanmalı
+# TagViewSet ve IngredientViewSet için tekrar tekrar kullandığımızdan aynı kodları bu class ile tek yerde topluyoruz.
+class BaseRecipeAttrViewSet(mixins.DestroyModelMixin,  # delete
+                            mixins.UpdateModelMixin,  # update
+                            mixins.ListModelMixin,
+                            viewsets.GenericViewSet):
+    """Base viewset for recipe attributes."""
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryset to authenticad user."""
+        return self.queryset.filter(user=self.request.user).order_by("-name")
+
+
+class TagViewSet(BaseRecipeAttrViewSet):  # mixins, generic viewset'ten önce tanımlanmalı
     """Manage tags in the database."""
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        """Filter queryset to authenticad user."""
-        return self.queryset.filter(user=self.request.user).order_by("-name")
 
 
-class IngredientViewSet(mixins.DestroyModelMixin,
-                        mixins.UpdateModelMixin,
-                        mixins.ListModelMixin,
-                        viewsets.GenericViewSet):
+class IngredientViewSet(BaseRecipeAttrViewSet):
     """Manage ingredients in the database."""
     serializer_class = serializers.IngredientSerializer
     queryset = Ingredient.objects.all()
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        """Filter queryset to authenticad user."""
-        return self.queryset.filter(user=self.request.user).order_by("-name")
 
