e261ac5c55496ba5fa0ae04c465ac1990e082b65
Emre Çalışır <emrecalisir95@gmail.com>
2023-04-09 14:29:30 +0300

    Implement filterin g for tags and ingredients

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,6 +2,7 @@
 Views for recipe APIs.
 """
 
+from drf_spectacular.utils import extend_schema_view, extend_schema, OpenApiParameter, OpenApiTypes
 from rest_framework import viewsets, mixins, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -11,6 +12,22 @@ from rest_framework.permissions import IsAuthenticated
 from core.models import Recipe, Tag, Ingredient
 from recipe import serializers
 
+@extend_schema_view(  # drf_spectacular'ın oluşturduğu OpenApi şemasına filtre için özellik ekliyor.
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "tags",
+                OpenApiTypes.STR,
+                description="Comma seperated list of tag IDs to filter."
+            ),
+            OpenApiParameter(
+                "ingredients",
+                OpenApiTypes.STR,
+                description="Comma seperated list of ingredient IDs to filter."
+            )
+        ]
+    )
+)
 class RecipeViewSet(viewsets.ModelViewSet):  # ModelViewSet -> modelleri ile çalışmka için kullanışlıdır. get, post, put, patch, ... methodlarını barındırır
     """View for manage recipe APIs."""
     serializer_class = serializers.RecipeDetailSerializer
@@ -18,9 +35,25 @@ class RecipeViewSet(viewsets.ModelViewSet):  # ModelViewSet -> modelleri ile ça
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
+    def _params_to_ints(self, qs):
+        """Convert a list of strings to integers."""
+        return [int(str_id) for  str_id in qs.split(",")]
+
     def get_queryset(self):  # Normalde queryset içindeki get tüm recipe'leri dönerdi fakat burada o methodu overwrite ederek user'a ait recipe'lerin gelmesini sağlayacak şekilde düzenliyoruz.
         """Retrieve recipes for authenticated user."""
-        return self.queryset.filter(user=self.request.user).order_by("-id")
+        tags = self.request.query_params.get("tags")
+        ingredients = self.request.query_params.get("ingredients")
+        queryset = self.queryset
+        if tags:
+            tag_ids = self._params_to_ints(tags)
+            queryset = queryset.filter(tags__id__in=tag_ids)
+        if ingredients:
+            ingredient_ids = self._params_to_ints(ingredients)
+            queryset = queryset.filter(ingredients__id__in=ingredient_ids)
+
+        return queryset.filter(
+            user=self.request.user
+        ).order_by("-id").distinct()
 
     # https://www.django-rest-framework.org/api-guide/generic-views/#get_serializer_classself
     def get_serializer_class(self):  # burada serialize class'ı çağırmayı overwrite ediyoruz. eğer list aksiyonu olursa RecipeSerializer eğer diğer bir aksiyon ise RecipeDetailSerializer dönmesini sağlayacak şekilde düzenliyoruz. Bu işlemi RecipeSerializer için yazdığımız kısımları RecipeDetailSerializer için tekrar yazmamak için yapıyoruz.
@@ -51,6 +84,17 @@ class RecipeViewSet(viewsets.ModelViewSet):  # ModelViewSet -> modelleri ile ça
 
 
 # TagViewSet ve IngredientViewSet için tekrar tekrar kullandığımızdan aynı kodları bu class ile tek yerde topluyoruz.
+@extend_schema_view(
+    list=extend_schema(
+        parameters = [
+            OpenApiParameter(
+                "assigned_only",
+                OpenApiTypes.INT, enum=[0, 1],  # sadece 0 ve 1 girilebilir. (False - True)
+                description="Filter by items assigned to recipes",
+            )
+        ]
+    )
+)
 class BaseRecipeAttrViewSet(mixins.DestroyModelMixin,  # delete
                             mixins.UpdateModelMixin,  # update
                             mixins.ListModelMixin,
@@ -61,7 +105,15 @@ class BaseRecipeAttrViewSet(mixins.DestroyModelMixin,  # delete
 
     def get_queryset(self):
         """Filter queryset to authenticad user."""
-        return self.queryset.filter(user=self.request.user).order_by("-name")
+        assigned_only = bool(
+            int(self.request.query_params.get("assigned_only", 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+        return queryset.filter(
+            user=self.request.user
+        ).order_by("-name").distinct()
 
 
 class TagViewSet(BaseRecipeAttrViewSet):  # mixins, generic viewset'ten önce tanımlanmalı
