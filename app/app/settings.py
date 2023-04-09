4fc29f143d350cb9ba18716af6ed1176b7808412
Emre Çalışır <emrecalisir95@gmail.com>
2023-04-10 00:02:17 +0300

    Update Django settings & fix typo

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,12 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ei%8hq2$qzr2xqx5g^!43w3#!&6ye#3wp1tuvhhkyi^k2e0e2)'
+SECRET_KEY = os.environ.get("SECRET_KEY", "changeme")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get("DEBUG", 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+    None,
+    os.environ.get("ALLOWED_HOSTS", "").split(","),
+    )
+)
 
 
 # Application definition
--- proxy/run.sh
+++ proxy/run.sh
@@ -4,4 +4,4 @@ set -e
 
 # environment substitute
 envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
-nginx -g "deamon off;"
+nginx -g "daemon off;"
